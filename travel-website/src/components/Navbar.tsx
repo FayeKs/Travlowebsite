4582f13206a9c81d51edfa38721527bba7baefe8
Faye Bishop <fayebishop44@gmail.com>
2025-04-23 23:02:39 -0500

    add top desinatins sectin and array and styling

--- travel-website/src/components/Navbar.tsx
+++ travel-website/src/components/Navbar.tsx
@@ -1,16 +1,34 @@
+import { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <nav className='fixed m-4 top-0 right-0 left-0 shadow-lg z-50 bg-white'>
       <div className='container px-4 flex justify-between items-center h-12'>
         <h3 className='text-2xl font-bold'>Travlo</h3>
-        <div className='flex space-x-4 text-sm  items-center'>
+        <div className='hidden md:flex space-x-4 text-sm  items-center'>
           <Link to="/" className='px-4 py-2 hover:bg-gray-600 hover:text-white' >Home</Link>
           <Link to="" className='px-4 py-2 hover:bg-gray-600 hover:text-white'>Contact</Link>
           <Link to="" className='px-4 py-2 hover:bg-gray-600 hover:text-white'>About</Link>
           <button className='py-2 px-6 border bg-gray-300'>Login</button>
         </div>
+        <div className='md:hidden'>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+      </div>
+      <div>
+    {isOpen && (
+      <div className='md:hidden flex space-y-6 py-4 flex-col bg-white items-center'>
+      <Link to="/" className='px-4 py-2 hover:bg-gray-600 hover:text-white' >Home</Link>
+      <Link to="" className='px-4 py-2 hover:bg-gray-600 hover:text-white'>Contact</Link>
+      <Link to="" className='px-4 py-2 hover:bg-gray-600 hover:text-white'>About</Link>
+      <button className='py-2 px-6 border bg-gray-300'>Login</button>
+    </div>
+    )}
       </div>
       
     </nav>
---
+++ travel-website/src/components/TopDestinations.tsx
@@ -0,0 +1,51 @@
+import amalfi from '../assets/images/amalfi.jpg'
+import london from '../assets/images/london.jpg'
+import nyc from '../assets/images/nyc.jpg'
+import tokyo from '../assets/images/tokyo.jpg'
+
+
+
+const TopDestinations = () => {
+    const destinations = [
+        {
+            image:  amalfi,
+            title: 'Amalfi Coast, Italy',
+            description: ''
+        },
+        {
+            image: nyc,
+            title: 'New York, USA',
+            description:''
+        },
+        {
+            image: london,
+            title: 'London, England',
+            description: ''
+        },
+        {
+            image: tokyo,
+            title: 'Tokyo, Japan',
+            description: ''
+        }
+    ]
+  return (
+    <div className='py-12 bg-gray-100'>
+        <div className='container mx-auto px-4'>
+            <h2 className='text-3xl font-bold text-center mb-8'>Top Destinations</h2>
+        </div>
+        <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {destinations.map((city, index) => (
+                <div key={index} className='bg-white rounded-lg shadow-md overflow-hidden cursor-pointer'>
+                    <img src={city.image} alt="" className='w-full h-48 object-cover transform transition duration-300 hover:scale-110'/>  
+                    <div className='p-4'>
+                        <h3 className='text-xl font-bold mb-2'>{city.title}</h3>
+                        <p>{city.description}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default TopDestinations
